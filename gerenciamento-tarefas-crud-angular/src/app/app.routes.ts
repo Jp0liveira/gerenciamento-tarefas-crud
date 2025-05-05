dbb946640c0a826163b294868f8968fb72c0d24b
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2025-05-05 16:55:48 -0300

    feat: incluindo a toolbar

--- gerenciamento-tarefas-crud-angular/src/app/app.routes.ts
+++ gerenciamento-tarefas-crud-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'tarefas' },
+  {
+    path: 'tarefas',
+    loadChildren: () => import('./tarefas/tarefas.routes').then(m => m.TAREFAS_ROUTES)
+  }
+];
---
+++ gerenciamento-tarefas-crud-angular/src/app/tarefas/tarefas.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import {TarefasComponent} from './tarefas/tarefas.component';
+
+export const TAREFAS_ROUTES: Routes = [
+  { path: '', component: TarefasComponent }
+];

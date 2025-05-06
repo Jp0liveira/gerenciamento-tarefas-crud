7ae8b59b6159119daa673afa918aa14e31cbe40f
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2025-05-06 10:14:04 -0300

    feat: inicializando o projeto gerenciamento tarefas crud spring

---
+++ gerenciamento-tarefas-crud-spring/src/main/java/com/accenture/gerenciamento_tarefas_crud_spring/GerenciamentoTarefasCrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.gerenciamento_tarefas_crud_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoTarefasCrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoTarefasCrudSpringApplication.class, args);
+	}
+
+}
